d34f402eb36a5d41fd589db5134083dca7d73e4f
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-01 18:46:52 -0300

    Add method to execute actions in module provider

--- lib/puppet/provider/module/module.rb
+++ lib/puppet/provider/module/module.rb
@@ -1,6 +1,12 @@
 Puppet::Type.type(:module).provide(:module) do
   desc "Implements DNF module actions"
 
+  commands :dnf => 'dnf'
+
+  def do_action(*params)
+    dnf('module', '-q', *params)
+  end
+
   def action
     true
   end
