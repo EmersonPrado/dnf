e669223b16530ab2f01a1e176ee21ed3ea58b5f9
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-02 15:06:37 -0300

    Add generic run function to module resource provider

--- lib/puppet/provider/module/module.rb
+++ lib/puppet/provider/module/module.rb
@@ -3,15 +3,15 @@ Puppet::Type.type(:module).provide(:module) do
 
   commands :dnf => 'dnf'
 
-  def do_action(*params)
-    dnf('module', '-q', *params)
-  end
-
   def query_module(module_name, *state)
     dnf('-q', 'module', *state, 'list', module_name).lines.each do |line|
       return true if line.split()[0] == module_name
     end
     return false
+  end
+
+  def run_action(command, module_name)
+    dnf('-y', 'module', command, module_name)
   end
 
   def action
