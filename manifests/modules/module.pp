fba9cbf48a6b0c09280c6ba431221db5e54b7635
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-03 14:54:23 -0300

    Add docstring for defined type dnf::modules::module

--- manifests/modules/module.pp
+++ manifests/modules/module.pp
@@ -1,3 +1,20 @@
+# Manage DNF modules
+#   To manage DNF module streams or profiles, use dnf::modules::(stream|profile)
+#
+# Usage:
+#   dnf::modules::module { '<Title>':
+#     module => '<Module>',
+#     action => '<Action>',
+#   }
+# Or
+#   dnf::modules::module { '<Title>':
+#     module => ['<Module 1>', '<Module 2>', ...],
+#     action => '<Action>',
+#   }
+#
+# DNF module actions:
+#   https://dnf.readthedocs.io/en/latest/command_ref.html#module-command-label
+#
 define dnf::modules::module (
   Variant[String, Array] $module,
   Enum[disable, enable, install, remove, reset, update] $action,
