d567d0ecb6f44aa8c42f8f303dd272ac711c18a0
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-03 16:31:31 -0300

    Add docstring for defined type dnf::modules::stream

--- manifests/modules/stream.pp
+++ manifests/modules/stream.pp
@@ -1,3 +1,16 @@
+# Manage DNF module streams
+#   To manage DNF modules or its profiles, use dnf::modules::(module|profile)
+#
+# Usage:
+#   dnf::modules::stream { '<Title>':
+#     module => '<Module>',
+#     stream => '<Stream>',
+#     action => 'enable' or 'switch-to',
+#   }
+#
+# DNF module actions:
+#   https://dnf.readthedocs.io/en/latest/command_ref.html#module-command-label
+#
 define dnf::modules::stream (
   String $module,
   String $stream,
