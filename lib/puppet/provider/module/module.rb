e1189c569b02da633fc6fd9afcac8448fc7e65ee
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-03 12:49:32 -0300

    Rename methods to (get|set)_module in module resource type

--- lib/puppet/provider/module/module.rb
+++ lib/puppet/provider/module/module.rb
@@ -3,7 +3,7 @@ Puppet::Type.type(:module).provide(:module) do
 
   commands :dnf => 'dnf'
 
-  def query_module(module_name, *state)
+  def get_module(module_name, *state)
     begin
       dnf('-q', 'module', *state, 'list', module_name)
     rescue
@@ -13,25 +13,25 @@ Puppet::Type.type(:module).provide(:module) do
     end
   end
 
-  def run_action(command, module_name)
+  def set_module(command, module_name)
     dnf('-y', 'module', command, module_name)
   end
 
   def action
-    raise "Resource #{resource[:name]} doesn't exist!" unless query_module(resource[:name])
+    raise "Resource #{resource[:name]} doesn't exist!" unless get_module(resource[:name])
   end
   def action=(action_name)
     case action_name
     when :enable,:disable
-      return if query_module(resource[:name], "--#{action_name}d")
+      return if get_module(resource[:name], "--#{action_name}d")
     when :reset
-      return unless query_module(resource[:name], "--enabled") or query_module(resource[:name], "--disabled")
+      return unless get_module(resource[:name], "--enabled") or get_module(resource[:name], "--disabled")
     when :install
-      return if query_module(resource[:name], "--installed")
+      return if get_module(resource[:name], "--installed")
     when :remove,:update
-      return unless query_module(resource[:name], "--installed")
+      return unless get_module(resource[:name], "--installed")
     end
-    run_action(action_name, resource[:name])
+    set_module(action_name, resource[:name])
   end
 
 end
