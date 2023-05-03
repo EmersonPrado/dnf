b8fcb808115c04ea2bfad4e939a2b3ebcd8edfaf
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-03 18:48:00 -0300

    [Rubocop] Fix spacings

--- lib/puppet/provider/module/module.rb
+++ lib/puppet/provider/module/module.rb
@@ -21,13 +21,13 @@ Puppet::Type.type(:module).provide(:module) do
 
   def action=(action_name)
     case action_name
-    when :enable,:disable
+    when :enable, :disable
       return if get_module(resource[:module], "--#{action_name}d")
     when :reset
       return unless get_module(resource[:module], '--enabled') or get_module(resource[:module], '--disabled')
     when :install
       return if get_module(resource[:module], '--installed')
-    when :remove,:update
+    when :remove, :update
       return unless get_module(resource[:module], '--installed')
     end
     set_module(action_name, resource[:module])
