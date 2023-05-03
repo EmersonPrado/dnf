3ac7cff267272115ad7877dd6771edb0d0abfad9
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-03 16:11:42 -0300

    Validate module and stream in stream resource provider

--- lib/puppet/provider/stream/stream.rb
+++ lib/puppet/provider/stream/stream.rb
@@ -7,8 +7,22 @@ Puppet::Type.type(:stream).provide(:stream) do
     dnf('-q', 'module', 'list', *state, item)
   end
 
-  def action
+  def exists?(module_name, stream_name)
+    begin
+      get(module_name)
+    rescue
+      raise "Module #{module_name} doesn't exist!"
+    end
+    begin
+      get("#{module_name}:#{stream_name}")
+    rescue
+      raise "Stream #{stream_name} from #{module_name} doesn't exist!"
+    end
     true
+  end
+
+  def action
+    exists?(resource[:module], resource[:stream])
   end
   def action=(action_name)
     true
