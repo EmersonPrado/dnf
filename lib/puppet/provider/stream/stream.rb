145a0abcf9ffbb031bc912c965e3bd813b05287b
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-03 16:27:39 -0300

    Implement stream change in stream provider

--- lib/puppet/provider/stream/stream.rb
+++ lib/puppet/provider/stream/stream.rb
@@ -34,6 +34,10 @@ Puppet::Type.type(:stream).provide(:stream) do
     end
   end
 
+  def set_stream(command, module_name, stream_name)
+    dnf('-y', 'module', command, "#{module_name}:#{stream_name}")
+  end
+
   def action
     exists?(resource[:module], resource[:stream])
   end
@@ -49,6 +53,7 @@ Puppet::Type.type(:stream).provide(:stream) do
     when :switch
       action_name = "switch-to"
     end
+    set_stream(action_name, resource[:module], resource[:stream])
   end
 
 end
