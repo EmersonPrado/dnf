41d5ec07aecac3d227d061070c5642fa7fb1cdfe
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-08 18:11:08 -0300

    Add dnf_module_stream type compile test

--- spec/unit/puppet/type/dnf_module_stream_spec.rb
+++ spec/unit/puppet/type/dnf_module_stream_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe 'dnf_module_stream custom resource type' do
   it 'loads' do
     expect(Puppet::Type.type(:dnf_module_stream)).not_to be_nil
   end
+
+  let(:params) do
+    {
+      'module' => 'nginx',
+      'stream' => '1.20',
+    }
+  end
+
+  ['enable', 'switch_to'].each do |action|
+    context "#{action} nginx module 1.20 stream" do
+      let(:params) do
+        { 'action' => action }
+      end
+
+      it is_expected.to compile
+    end
+  end
 end
