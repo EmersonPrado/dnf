5d9231b7e7d48764123aae3944515947c64e84e6
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-08 18:09:32 -0300

    Add dnf_module type compile test without profile

--- spec/unit/puppet/type/dnf_module_spec.rb
+++ spec/unit/puppet/type/dnf_module_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe 'dnf_module custom resource type' do
   it 'loads' do
     expect(Puppet::Type.type(:dnf_module)).not_to be_nil
   end
+
+  let(:params) do
+    { 'module' => 'nginx' }
+  end
+
+  ['disable', 'enable', 'install', 'remove', 'reset', 'update'].each do |action|
+    context "#{action} nginx module" do
+      let(:params) do
+        {
+          'profile' => :undef,
+          'action'  => action,
+        }
+      end
+
+      it is_expected.to compile
+    end
+  end
 end
