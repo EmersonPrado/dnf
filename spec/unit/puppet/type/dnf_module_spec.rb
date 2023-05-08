a4faefafae2ffd2916fd99359947580d6af8f354
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-08 18:09:58 -0300

    Add dnf_module type compile test with profile

--- spec/unit/puppet/type/dnf_module_spec.rb
+++ spec/unit/puppet/type/dnf_module_spec.rb
@@ -24,4 +24,17 @@ RSpec.describe 'dnf_module custom resource type' do
       it is_expected.to compile
     end
   end
+
+  ['install', 'remove', 'update'].each do |action|
+    context "#{action} nginx module common profile" do
+      let(:params) do
+        {
+          'profile' => 'common',
+          'action'  => action,
+        }
+      end
+
+      it is_expected.to compile
+    end
+  end
 end
