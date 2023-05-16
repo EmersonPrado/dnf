b9c82d78c767b899235867c8e6ff5d47ed512e8e
Emerson Prado <emerson.prado.eng@gmail.com>
2023-05-16 16:19:02 -0300

    Unit test custom type parameters

--- spec/unit/puppet/type/dnf_module_spec.rb
+++ spec/unit/puppet/type/dnf_module_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe dnf_module do
   it 'loads' do
     expect(dnf_module).not_to be_nil
   end
+
+  let :params do
+    [
+      :title,
+      :module,
+      :profile,
+    ]
+  end
+
+  it 'has expected parameters' do
+    params.each do |param|
+      expect(dnf_module.parameters).to be_include(param)
+    end
+  end
 end
--- spec/unit/puppet/type/dnf_module_stream_spec.rb
+++ spec/unit/puppet/type/dnf_module_stream_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe dnf_module_stream do
   it 'loads' do
     expect(dnf_module_stream).not_to be_nil
   end
+
+  let :params do
+    [
+      :title,
+      :module,
+      :stream,
+    ]
+  end
+
+  it 'has expected parameters' do
+    params.each do |param|
+      expect(dnf_module_stream.parameters).to be_include(param)
+    end
+  end
 end
